1e1e5759028a42ca4d37bdbd5a6c968f451f3508
vera_sidarovich <vera_sidarovich@epam.com>
2015-02-03 18:05:27 +0300

    made some changes

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,16 +2,16 @@
  * Created by Vera_Sidarovich on 2/3/2015.
  */
 public class Percolation {
-    private int[][] array;
+    private int[][] sites;
 
     private QuickFindUF uf;
 
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
-        array = new int[N][N];
+        sites = new int[N][N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                array[i][j] = -1;
+                sites[i][j] = 0;
             }
         }
         uf = new QuickFindUF(N * N);
@@ -19,40 +19,42 @@ public class Percolation {
 
     // open site (row i, column j) if it is not open already
     public void open(int i, int j) {
-        int currentIndex = (array.length - 1) * j + i;
+        System.out.println("i = " + i + "  j = " + j);
+        int currentIndex = (sites.length - 1) * j + i;
+        sites[i][j] = 1;
 
-        int leftShift = i - 1;
-        int leftIndex = (array.length - 1) * j + leftShift;
+        int upShift = i - 1;
+        int upIndex = (sites.length - 1) * j + upShift;
 
-        if (leftIndex > 0 && leftIndex < uf.count() && isOpen(leftIndex, j)) {
-            uf.union(currentIndex, leftIndex);
+        if (upIndex >= 0 && upIndex < uf.count() && isOpen(upShift, j)) {
+            uf.union(currentIndex, upIndex);
         }
 
-        int rightShift = i + 1;
-        int rightIndex = (array.length - 1) * j + rightShift;
+        int downShift = i + 1;
+        int downIndex = (sites.length - 1) * j + downShift;
 
-        if (rightIndex > 0 && rightIndex < uf.count() && isOpen(rightIndex, j)) {
-            uf.union(currentIndex, rightIndex);
-        }
-
-        int downShift = j + 1;
-        int downIndex = (array.length - 1) * downShift + i;
-
-        if (downIndex > 0 && downIndex < uf.count() && isOpen(downIndex, j)) {
+        if (downIndex >= 0 && downIndex < uf.count() && isOpen(downIndex, j)) {
             uf.union(currentIndex, downIndex);
         }
 
-        int upShift = j - 1;
-        int upIndex = (array.length - 1) * upShift + i;
+        int rightShift = j + 1;
+        int rightIndex = (sites.length - 1) * rightShift + i;
 
-        if (upIndex > 0 && upIndex < uf.count() && isOpen(upIndex, j)) {
-            uf.union(currentIndex, upIndex);
+        if (rightIndex >= 0 && rightIndex < uf.count() && isOpen(i, rightShift)) {
+            uf.union(currentIndex, rightIndex);
+        }
+
+        int leftShift = j - 1;
+        int leftIndex = (sites.length - 1) * leftShift + i;
+
+        if (leftIndex >= 0 && leftIndex < uf.count() && isOpen(i, leftShift)) {
+            uf.union(currentIndex, leftIndex);
         }
     }
 
     // open site (row i, column j) if it is not open already
     public boolean isOpen(int i, int j) {
-        return false;
+        return sites[i][j] == 1;
     }
 
     // is site (row i, column j) full?
@@ -71,10 +73,14 @@ public class Percolation {
         Percolation percolation = new Percolation(randomArraySize);
 
         while (!percolation.percolates()) {
-            int randomRow = (int) Math.random() * randomArraySize;
-            int randomColumn = (int) Math.random() * randomArraySize;
-            if (!percolation.isOpen(randomRow, randomColumn)) {
-                percolation.open(randomRow, randomColumn);
+            int randomRow = (int) (Math.random() * randomArraySize);
+            int randomColumn = (int) (Math.random() * randomArraySize);
+            try {
+                if (!percolation.isOpen(randomRow, randomColumn)) {
+                    percolation.open(randomRow, randomColumn);
+                }
+            } catch (Exception e) {
+                System.out.println("Error!");
             }
         }
     }
